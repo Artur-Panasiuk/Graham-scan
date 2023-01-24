88fc661d40aa472f42bc3d32234d7eea4a683f07
Artur-Panasiuk <47883583+Artur-Panasiuk@users.noreply.github.com>
2023-01-24 16:21:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,26 +10,33 @@
 
 using namespace std;
 
-struct points{
+struct points {
     int i;
     double x, y, angle;
 };
 
-double vectorCalc(points p0, points p1){
-    return p1.y*p0.x-p0.y*p1.x;
+int ori(const void *pp1, const void *pp2) {
+    points* p1 = (points*)pp1;
+    points* p2 = (points*)pp2;
+    double temp = p2->x * p1->y - p1->x * p2->y;
+    if (temp > 0) 
+        return 1;
+    else if (temp < 0) 
+        return -1;
+    return 0;
 }
 
-double calc(points p1, points p0){
-    return sqrt(pow(p1.x-p0.x,2)+pow(p1.y-p0.y,2));
+
+double orientation(points p1, points p2, points p3) {
+    points v1;
+    v1.x = p2.x - p1.x;
+    v1.y = p2.y - p1.y;
+    points v2;
+    v2.x = p3.x - p2.x;
+    v2.y = p3.y - p2.y;
+    return v2.x * v1.y - v1.x * v2.y;
 }
 
-bool compare(points p0, points p1){
-    return p0.angle > p1.angle;
-}
-
-double orientation(points p1, points p2, points p3){
-    return (p3.y - p2.y)*(p2.x - p1.x) - (p2.y - p1.y)*(p3.x - p2.x);
-}
 
 int main()
 {
@@ -37,33 +44,33 @@ int main()
 
     mainStart = clock();
 
-    points *collection;
-    int collectionSize;
+    points* collection;
+    double collectionSize;
 
     //OPEN FILE-------------------------------------
 
     fstream cordsFile;
-    cordsFile.open("points0.txt", ios::in);
+    cordsFile.open("points5.txt", ios::in);
 
-    if(cordsFile.is_open()){
+    if (cordsFile.is_open()) {
         string temp, line;
         getline(cordsFile, temp);
         int numOfLines = stoi(temp);
         collectionSize = numOfLines;
 
         collection = new points[numOfLines];
-        double *allCords = new double[numOfLines*2];
+        double* allCords = new double[numOfLines * 2];
         int i = 0;
 
 
-        while(getline(cordsFile, line)){
+        while (getline(cordsFile, line)) {
             stringstream s(line);
             string cordString;
-            while(s >> cordString){
+            while (s >> cordString) {
                 allCords[i++] = stod(cordString);
             }
             int inner = 0;
-            for(int j = 0; j < numOfLines; j++){
+            for (int j = 0; j < numOfLines; j++) {
                 collection[j].x = allCords[inner++];
                 collection[j].y = allCords[inner++];
             }
@@ -71,21 +78,26 @@ int main()
         cordsFile.close();
         delete[] allCords;
     }
+    else {
+        cout << "UNABLE TO OPEN FILE" << endl;
+        return 0;
+    }
 
     //FIND STARTING POINT-------------------------------------
 
-    points *collectionCopy = new points[collectionSize];
+    points* collectionCopy = new points[collectionSize];
 
-    double temp = 1;
-    int startIndex;
-    for(int i = 0; i < collectionSize; i++){
+    int startIndex = 0;
+    double temp = collection[startIndex].y;
+    for (int i = 0; i < collectionSize; i++) {
         collectionCopy[i] = collection[i];
         collectionCopy[i].i = i; //DO WRZUCENIA NA SORTOWANIE
-        if(collectionCopy[i].y < temp){
+        if (collectionCopy[i].y < temp) {
             temp = collectionCopy[i].y;
             startIndex = i;
-        }else if(collectionCopy[i].y == temp){
-            if(collectionCopy[i].x < collectionCopy[startIndex].x){
+        }
+        else if (collectionCopy[i].y == temp) {
+            if (collectionCopy[i].x < collectionCopy[startIndex].x) {
                 temp = collectionCopy[i].y;
                 startIndex = i;
             }
@@ -100,18 +112,7 @@ int main()
 
     vector<points> hull;
 
-//    for(int i = 1; i < collectionSize; i++){
-//        collectionCopy[i].angle =
-//    }
-//
-//    sort(collectionCopy + 1, collectionCopy + collectionSize, compare);
-//
-    swap(collectionCopy[1], collectionCopy[7]);
-    swap(collectionCopy[2], collectionCopy[5]);
-    swap(collectionCopy[3], collectionCopy[4]);
-    swap(collectionCopy[4], collectionCopy[6]);
-    swap(collectionCopy[5], collectionCopy[6]);
-    swap(collectionCopy[6], collectionCopy[7]);
+    qsort(collectionCopy + 1, collectionSize - 1, sizeof(points), ori);
 
     sortEnd = clock();
 
@@ -121,9 +122,10 @@ int main()
 
     hull.push_back(collectionCopy[0]);
     hull.push_back(collectionCopy[1]);
+    
 
-    for(int i = 2; i < collectionSize; i++){
-        if(orientation(hull[i-2], hull[-1], collectionCopy[i]) > 0){
+    for (int i = 2; i < collectionSize; i++) {                
+        while(orientation(hull[hull.size() - 2], hull[hull.size() - 1], collectionCopy[i]) > 0) {
             hull.pop_back();
         }
         hull.push_back(collectionCopy[i]);
@@ -134,23 +136,25 @@ int main()
     delete[] collection;
     delete[] collectionCopy;
 
+    //OUTPUT INFO-------------------------------------------------------
+
     mainEnd = clock();
 
     cout << "Liczba punktow bioraca udzial w powloce: " << hull.size() << endl;
     cout << "Indeksy budujace powloke: " << endl;
 
-    for(points i : hull){
+    for (points i : hull) {
         cout << "index: " << i.i << " x: " << i.x << " y: " << i.y << endl;
     }
     cout << endl;
 
-    double time1 = ((double)(mainEnd-mainStart)) / CLOCKS_PER_SEC;
-    double time2 = ((double)(sortEnd-sortStart)) / CLOCKS_PER_SEC;
-    double time3 = ((double)(assignEnd-assignStart)) / CLOCKS_PER_SEC;
+    double time1 = ((double)(mainEnd - mainStart)) / CLOCKS_PER_SEC;
+    double time2 = ((double)(sortEnd - sortStart)) / CLOCKS_PER_SEC;
+    double time3 = ((double)(assignEnd - assignStart)) / CLOCKS_PER_SEC;
 
-    cout << "Czas sortowania: " << time2 << "s" <<  endl;
-    cout << "Czas przypisania: " << time3 << "s" <<  endl;
-    cout << "Czas calego programu: " << time1 << "s" <<  endl;
+    cout << "Czas sortowania: " << time2 << "s" << endl;
+    cout << "Czas przypisania: " << time3 << "s" << endl;
+    cout << "Czas calego programu: " << time1 << "s" << endl;
 
     return 0;
 }
